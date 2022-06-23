bc61d2199fa33a61e8fdf08b3b2e37608ab1d106
JoeIt <josignaciot@gmail.com>
2022-06-23 17:18:05 -0300

    Functionality Game

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import { MouseEvent } from 'react';
+// Types
+import { AnswerObject } from '../App';
+// Styles
+// import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 type Props = {
   question: string;
   answers: string[];
-  callback: any;
-  userAnswer: any;
+  callback: (e: MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNr: number;
   totalQuestions: number;
 };
@@ -25,8 +29,12 @@ const QuestionCard: React.FC<Props> = ({
       <p dangerouslySetInnerHTML={{ __html: question }} />
       <div>
         {answers.map(answer => (
-          <div>
-            <button disabled={userAnswer} onClick={callback}>
+          <div key={answer}>
+            <button
+              disabled={userAnswer ? true : false}
+              value={answer}
+              onClick={callback}
+            >
               <span dangerouslySetInnerHTML={{ __html: answer }} />
             </button>
           </div>
